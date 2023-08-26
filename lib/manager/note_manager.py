cea50afd2f5109af8849232c621ae6714f8a38c9
Pawankashap <126440752+Pawankashap@users.noreply.github.com>
2023-08-25 18:28:14 -0700

    update menus

--- lib/manager/note_manager.py
+++ lib/manager/note_manager.py
@@ -1,5 +1,6 @@
 from db.models import Session, User, Note, Tag
 from cursesmenu import CursesMenu
+from sqlalchemy.orm import sessionmaker
 from cursesmenu.items import FunctionItem
 import click
 
@@ -9,7 +10,6 @@ class NoteManager:
     def __init__(self):
             self.session = Session()
 
-    # def add_note(self, username, title, content, tags):
     def add_note(self):
             notes_list = []
             tags_dict = {}
@@ -30,9 +30,10 @@ class NoteManager:
                         tag = Tag(name=tag_name)
                     note.tags.append(tag)
                 self.session.commit()
-                print("Note added successfully!")
+                print(f"Note added successfully!")
             else:
                 print(f"User {username} not found.")
+            input("Press Enter to return to the main menu...")
 
     def show_notes(self):
         session = Session()
@@ -42,11 +43,17 @@ class NoteManager:
         results = query.all()
 
         for note, username in results:
-            click.echo(f"Note ID: {note.id} , User: {username} , Title: {note.title} , Content: {note.content} , Tags: {', '.join(tag.name for tag in note.tags)}")
+            # click.echo(f"Note ID: {note.id} , User: {username} , Title: {note.title} , Content: {note.content} , Tags: {', '.join(tag.name for tag in note.tags)}")
+            print(f"Note ID: {note.id} , User: {username} , Title: {note.title} , Content: {note.content} , Tags: {', '.join(tag.name for tag in note.tags)}")
  
         self.session.close()    
-    
-    def edit(self,note_id, title, content, tags):
+        input("Press Enter to return to the main menu...")
+    def edit(self):
+
+        note_id = input("Enter Note ID: ")
+        title = input("Enter Note Title: ")
+        content = input("Enter Note Content: ")
+        tags = input("Enter Tag: ")
         note = self.session.query(Note).get(note_id)
         if not note:
             click.echo('Note not found!')
@@ -65,8 +72,10 @@ class NoteManager:
         self.session.close()
 
         click.echo('Note edited successfully!')
+        input("Press Enter to return to the main menu...")
 
-    def delete(self,note_id):
+    def delete(self):
+        note_id = input("Enter Note ID: ")
         note = self.session.query(Note).get(note_id)
         if not note:
             click.echo('Note not found!')
@@ -78,7 +87,8 @@ class NoteManager:
         self.session.close()
 
         click.echo('Note deleted successfully!')
-
+        input("Press Enter to return to the main menu...")
+        
     def interactive(self):
         notes = self.session.query(Note).all()
         if not notes:
