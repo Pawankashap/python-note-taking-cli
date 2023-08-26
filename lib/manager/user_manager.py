56db0e4d17710944dc61f5924d2b210523a02c72
Pawankashap <126440752+Pawankashap@users.noreply.github.com>
2023-08-25 18:01:27 -0700

    adds add-note in menu

--- lib/manager/user_manager.py
+++ lib/manager/user_manager.py
@@ -5,8 +5,10 @@ class UserManager:
     def __init__(self):
         self.session = Session()
 
-    def create_user(self,username):
+    def create_user(self):
+        username = input("Enter user's name: ")
         user = User(username=username)
         self.session.add(user)
         self.session.commit()
         print(f"User {username} created!")
+        
