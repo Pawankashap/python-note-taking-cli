6d0389554b730c6841c412da3d33f35d89588d06
Pawankashap <126440752+Pawankashap@users.noreply.github.com>
2023-08-26 01:08:48 -0700

    updates validation in user_manager.py

--- lib/manager/user_manager.py
+++ lib/manager/user_manager.py
@@ -7,8 +7,11 @@ class UserManager:
 
     def create_user(self):
         username = input("Enter user's name: ")
-        user = User(username=username)
-        self.session.add(user)
-        self.session.commit()
-        print(f"User {username} created!")
+        if not username:
+            print("User name cannot be empty. Please try again.")
+        else:
+            user = User(username=username)
+            self.session.add(user)
+            self.session.commit()
+            print(f"User {username} created!")
         input("Press Enter to return to the main menu...")
